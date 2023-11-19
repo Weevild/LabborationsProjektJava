cf1d06b3707742a2cd404fb5ea522ba3fb65cd0e
luumen <filip.westman@gmail.com>
2023-11-19 23:01:14 +0100

    Lade till kommentarer för att förtydliga.

--- src/CarTransport.java
+++ src/CarTransport.java
@@ -18,16 +18,17 @@ public class CarTransport extends Vehicles_with_platform implements StorageThing
     @Override
     public void storeVehicle(){
     }
-
+    // Stores an object (vehicle) if the absolute value of the distance between vehicle and transporter is below 10 and that the platform is in "down"-position.
     public void storeVehicle(Car vehicle) {
         if (Math.abs(vehicle.getXCoordinate()) - Math.abs(this.getXCoordinate()) <= 10  && Math.abs(vehicle.getYCoordinate()) - Math.abs(this.getYCoordinate()) <= 10 && !(vehicle instanceof CarTransport) && getFixedPlatformPosition()) {
             getStorage().add(vehicle);
         }
     }
+    // Implements removeVehicle() since it's obligatory.
     @Override
     public void removeVehicle(){
     }
-
+    // Performs the actual removal of a vehicle from the platform and places it within reasonable distance of transporter.
     public Car removeLastVehicle(){
         if (!getStorage().isEmpty() && getFixedPlatformPosition()) {
             int index = getStorage().size() - 1;
@@ -45,6 +46,7 @@ public class CarTransport extends Vehicles_with_platform implements StorageThing
         }
         return null;
     }
+    // Checks so that either an angled or fixed platform is in their lowered position
     @Override
     public boolean canMoveCheck(){
         return getPlatformPosition() == maxAngle || getFixedPlatformPosition();
--- src/Vehicles_with_platform.java
+++ src/Vehicles_with_platform.java
@@ -3,38 +3,26 @@ import java.util.List;
 
 public abstract class Vehicles_with_platform extends Car implements Movable {
 
-    private double platformAngle;
+    private double platformAngle; // For vehicles with adjustable platform
     public int maxAngle;
     public int minAngle;
-    private boolean FixedPlatformRetractedPosition;
-    private List<Car> storage = new ArrayList<>();
+    private boolean FixedPlatformRetractedPosition; // For vehicles with a platform with only up/down positions, checks if platform is "down".
+    private List<Car> storage = new ArrayList<>(); // List of stored objects (vehicles), the "platform" if you will.
 
-    public boolean getFixedPlatformPosition(){
-        return FixedPlatformRetractedPosition;
-    }
-    public void setFixedPlatformPosition(boolean position){
-        this.FixedPlatformRetractedPosition = position;
-    }
-
+    public boolean getFixedPlatformPosition(){ return FixedPlatformRetractedPosition; } // Getter for platform position
+    public void setFixedPlatformPosition(boolean position){ this.FixedPlatformRetractedPosition = position; } // Setter for platform position
     public List<Car> getStorage(){
         return storage;
-    }
-
-    /* Onödig kod, storeVehicle gör det bättre
-    public void setStorage(Car car){
-        if (!(car instanceof CarTransport)) {
-            this.storage.add(car);
-        }
-    }
-    */
-    public Vehicles_with_platform(){   // initializes platform angle as the lowest possible angle
+    } // Getter for storage list
+    public Vehicles_with_platform(){   // Initializes platform angle as the lowest possible angle
         platformAngle = minAngle;
     }
 
-    public abstract boolean canMoveCheck();
-    public double getPlatformPosition() {
+    public abstract boolean canMoveCheck(); // Keeps track of whether vehicle with platform can move
+    public double getPlatformPosition() { // Getter for platform angle
         return platformAngle;
     }
+    // Takes angle degrees and raises the platform with said degrees
     public void platformUp(double amount) {
         if (!canMoveCheck()) {
             double newAngle = Math.min(getPlatformPosition() + amount, maxAngle);
@@ -43,6 +31,7 @@ public abstract class Vehicles_with_platform extends Car implements Movable {
             }
         }
     }
+    // Takes angle degrees and lowers the platform with said degrees
     public void platformDown(double amount) {
         if (!canMoveCheck()) {
         double newAngle  = Math.max(getPlatformPosition() - amount, minAngle);
@@ -51,6 +40,7 @@ public abstract class Vehicles_with_platform extends Car implements Movable {
         }
         }
     }
+    // When called moves the vehicle if canMoveCheck conditions are met
     @Override
     public void move(){
         if (canMoveCheck()){
