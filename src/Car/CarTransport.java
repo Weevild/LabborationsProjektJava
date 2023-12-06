f603e728a7c391822f0be6b6766ee0e692f22935
luumen <filip.westman@gmail.com>
2023-12-06 19:02:57 +0100

    Converted FixedPlatform's and CarTransports relationship into composition based instead of inheritance.

--- src/Car/CarTransport.java
+++ src/Car/CarTransport.java
@@ -6,26 +6,29 @@ import java.util.List;
 
 import static java.lang.Math.abs;
 
-public class CarTransport extends FixedPlatform implements Loadable<Car>{
+public class CarTransport extends Car implements Loadable<Car>{
+    // Creates the instance variable to make use of composition and therefore FixedPlatform's contents
+    private FixedPlatform fixedPlatform;
     private List<Car> storage = new ArrayList<>(); // List of stored objects (vehicles), the "platform" if you will.
 
     public CarTransport(double x, double y){
-        super(2,400,x,y,false,"Car.Car Transport");
+        super(2,400,x,y,false,"Car Transport");
+        fixedPlatform = new FixedPlatform();
         color = Color.cyan;
-        setFixedPlatformPosition(true);
+        fixedPlatform.setFixedPlatformPosition(true);
         stopEngine();
     }
     @Override
     // Stores an object (vehicle) if the absolute value of the distance between vehicle and transporter is below 10 and that the platform is in "down"-position.
     public void storeVehicle(Car vehicle) {
-        if (Math.abs(vehicle.getXCoordinate()) - Math.abs(this.getXCoordinate()) <= 10  && Math.abs(vehicle.getYCoordinate()) - Math.abs(this.getYCoordinate()) <= 10 && !(vehicle instanceof CarTransport) && getFixedPlatformPosition()) {
+        if (Math.abs(vehicle.getXCoordinate()) - Math.abs(this.getXCoordinate()) <= 10  && Math.abs(vehicle.getYCoordinate()) - Math.abs(this.getYCoordinate()) <= 10 && !(vehicle instanceof CarTransport) && fixedPlatform.getFixedPlatformPosition()) {
             setIsStored(true);
             storage.add(vehicle);
         }
     }
     // Performs the removal of a vehicle from the platform and places it within reasonable distance of transporter.
     public Car removeLastVehicle(){
-        if (!storage.isEmpty() && getFixedPlatformPosition()) {
+        if (!storage.isEmpty() && fixedPlatform.getFixedPlatformPosition()) {
             int index = storage.size() - 1;
             Car car = storage.get(index); // Get the last car
 
@@ -43,12 +46,6 @@ public class CarTransport extends FixedPlatform implements Loadable<Car>{
         }
         return null;
     }
-    // Checks so that either a fixed platform is in their lowered position
-    @Override
-    public boolean cannotMove(){
-        return getFixedPlatformPosition();
-    }
-
     @Override
     public int getAmountOfVehicle() {
         return storage.size();
@@ -61,6 +58,14 @@ public class CarTransport extends FixedPlatform implements Loadable<Car>{
             car.setXCoordinate(this.getXCoordinate());
             car.setYCoordinate(this.getYCoordinate());
         }
+    }
+    @Override
+    public void turnLeft(){
+        if (!fixedPlatform.cannotMove()) super.turnLeft();
+    }
+    @Override
+    public void turnRight(){
+        if (!fixedPlatform.cannotMove()) super.turnRight();
     }
 
 }
--- src/Car/FixedPlatform.java
+++ src/Car/FixedPlatform.java
@@ -1,31 +1,11 @@
 package Car;
 
-public class FixedPlatform extends Car{
-    public FixedPlatform(int nr, int power, double x, double y, boolean stored, String model) {
-        super(nr, power, x, y, stored, model);
-    }
-
+public class FixedPlatform{
     private boolean FixedPlatformRetractedPosition; // For vehicles with a platform with only up/down positions, checks if platform is "down".
     public boolean getFixedPlatformPosition(){ return FixedPlatformRetractedPosition; } // Getter for platform position
     public void setFixedPlatformPosition(boolean position){ this.FixedPlatformRetractedPosition = position; } // Setter for platform position
 
     public boolean cannotMove() {
         return getFixedPlatformPosition();
-    }
-
-    // When called moves the vehicle if canMoveCheck conditions are met
-    @Override
-    public void move(){
-        if (!cannotMove()){
-            super.move();
-        }
-    }
-    @Override
-    public void turnLeft(){
-        if (!cannotMove()) super.turnLeft();
-    }
-    @Override
-    public void turnRight(){
-        if (!cannotMove()) super.turnRight();
     }
 }
